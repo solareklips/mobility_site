e40a551d01ce53c54a47943bea81cd8d276c9f6d
solareklips <117949030+solareklips@users.noreply.github.com>
2023-08-10 18:42:13 +0000

    Added picture 2

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 JOBS = [
   {
     'id' : 1,
-    'title' : 'Data Analyst',
+    'title' : 'Equipment',
     'location' : 'Bengaluru, India',
     'salary' : 'Rs. 20,00,000'
   },
   {
     'id' : 2,
-    'title' : 'Backend Engineer',
+    'title' : 'Muscles',
     'location' : 'Delhi, India',
     'salary' : 'Rs. 25,00,000'
   },
   {
     'id' : 3,
-    'title' : 'Clown Engineer',
+    'title' : 'Musclegrups',
     'location' : 'Junlge, Amazonas'
   },
 ]
@@ -27,9 +27,12 @@ def hello_world():
     return render_template('home.html',
                           jobs=JOBS)
 
-@app.route("/jobs")
+@app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
+# kommet til 1:51:18 i video tutorialen
+# https://www.youtube.com/watch?v=yBDHkveJUf4&t=6447s
