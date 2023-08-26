148d69ad014474e039470a7c9f7eddfb78fb8487
solareklips <117949030+solareklips@users.noreply.github.com>
2023-08-26 19:44:35 +0000

    updated

--- database.py
+++ database.py
@@ -1,13 +1,37 @@
 from sqlalchemy import create_engine, text
+"""
+server = "aws.connect.psdb.cloud"
+pw = "pscale_pw_TYQtOwUJPLPMY0iV9sqqZjEf3ZwwnyWbfMXT3jK3ULj"
+user = "b607qgzbnaiunae54uuw"
+"""
 
-connection_db_string = "mysql+pymysql://wzqa20h70d2gjrqf422t:pscale_pw_hQAnZz8zoJBBQyYU21ncpgutL2XE8rQSDyfcBKWXGlc@aws.connect.psdb.cloud/mobility?charset=utf8mb4"
+connection_db_string = "mysql+pymysql://b607qgzbnaiunae54uuw:pscale_pw_TYQtOwUJPLPMY0iV9sqqZjEf3ZwwnyWbfMXT3jK3ULj@aws.connect.psdb.cloud/mobility?charset=utf8mb4"
 
-engine = create_engine(connection_db_string, connect_args={"ssl"{
 
-}
-}
-)
+engine = create_engine(
+  connection_db_string, 
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  })
 
-with engine.connect() as connection:
-  result = connection.execute(text("select * from exercises"))
-  print(result.all())
+
+with engine.connect() as conn:
+  result = conn.execute(text("SELECT * FROM exercises"))
+  print("result.all(): ",result.all())
+  print("type(result): ",type(result))
+  result_all = result.all()
+  print("type(result_all): ",type(result_all))
+  print("result_all: ",result_all)
+"""
+  print(f"result: {result}")
+  print("type(result): ", type(result))
+  result_all = result.all()
+  print("type(result.all()): ", type(result_all))
+  first_result = result_all[0]
+  print("type(first_result): ", type(first_result))
+  first_result_dict = dict(first_result[0])
+  print("type(first_result_dict): ", type(first_result_dict))
+  print(first_result_dict)
+"""
